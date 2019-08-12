232c93bbda92148af594f3faf81ebff98a7998b2
Sanjay Suresh/JAM/Brenntag <sanjaysuresh15@gmail.com>
2019-08-11 20:58:11 -0500

    add 1986

--- hw9.sql
+++ hw9.sql
@@ -73,7 +73,8 @@ select employees.emp_no,
 	inner join salaries on
 	employees.emp_no = salaries.emp_no;
 	
-	
-
-	
+select *
+	from employees
+	where hire_date between '1986-01-01' and '1986-12-31'
+;
 
