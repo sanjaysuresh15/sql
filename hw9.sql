74e8c4a90960c2b30c58e50c6c8c17b64d892fd3
Sanjay Suresh/JAM/Brenntag <sanjaysuresh15@gmail.com>
2019-08-13 02:25:19 -0500

    question 8

--- hw9.sql
+++ hw9.sql
@@ -64,6 +64,7 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+--question 1
 select employees.emp_no, 
 	employees.first_name, 
 	employees.last_name, 
@@ -73,11 +74,13 @@ select employees.emp_no,
 	inner join salaries on
 	employees.emp_no = salaries.emp_no;
 	
+--question 2
 select *
 	from employees
 	where hire_date between '1986-01-01' and '1986-12-31'
 ;
 
+--question 3
 select dept_manager.dept_no,
 	dept_manager.emp_no,
 	dept_manager.from_date,
@@ -91,7 +94,7 @@ select dept_manager.dept_no,
 	join employees on
 	dept_manager.emp_no = employees.emp_no;
 	
-
+--question 4
 select dept_emt.emp_no,
 	departments.dept_name,
 	employees.last_name,
@@ -102,10 +105,12 @@ select dept_emt.emp_no,
 	full join employees on
 	dept_emt.emp_no = employees.emp_no;
 
+--question 5
 select * 
 	from employees 
 	where first_name = 'Hercules' and last_name like 'B%'
 
+--question 6
 select dept_emt.emp_no,
 	departments.dept_name,
 	employees.last_name,
@@ -116,7 +121,8 @@ select dept_emt.emp_no,
 	full join employees on
 	dept_emt.emp_no = employees.emp_no
 	where dept_name = 'Sales';
-	
+
+--question 7
 select dept_emt.emp_no,
 	departments.dept_name,
 	employees.last_name,
@@ -127,5 +133,10 @@ select dept_emt.emp_no,
 	full join employees on
 	dept_emt.emp_no = employees.emp_no
 	where dept_name = 'Sales' or dept_name = 'Development';
-	
+
+-- question 8
+select last_name, count(last_name) as "name count"
+	from employees
+	group by last_name
+	order by "name count" Desc;
 	
